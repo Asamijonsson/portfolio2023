b23a21d1f7a60dd2979d796465efa0a03387b5c3
Asami <90178926+Asamijonsson@users.noreply.github.com>
2023-05-10 23:38:28 +0200

    added animation in home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,57 @@
-
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import foodmain from "../image/foodoriginal.re.jpg";
 
 import style from "./Home.module.css";
 
 const Home = () => {
-  
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className={style.herobackground} id="main-content">
-      <legend>
-      <img src={foodmain} alt="" className={style.img} />
+    <div  id="main-content">
+      <legend className={style.herobackground}>
+        <img src={foodmain} alt="" className={style.img} />
       </legend>
-      <section className={style.secondsecbackground}>
-<h3>Welcome to my website!</h3>
-
-
-<article></article>
-      </section>
+      <article className={style.article}>
+        <motion.div
+        //  variants={{
+        //   open: { opacity: 1, height: "auto" },
+        //   collapsed: { opacity: 0, height: 0 }
+        // }}
+        // transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+          Layout
+          onClick={() => setIsOpen(!isOpen)}
+          className={style.card}
+          style={{
+            borderRadius: "1rem",
+            boxshadow: "0px 10px 30px rgba(0,0,0,0.5)",
+          }}
+        >
+          <motion.h2 Layout="position">Welcome to my website! </motion.h2>
+          <AnimatePresence>
+          {isOpen && (
+            <motion.section
+            key="content"
+            initial="collapsed"
+            animate="open"
+            exit="collapsed"
+              variants={{
+                open: { opacity: 1, height: "auto" },
+                collapsed: { opacity: 0, height: 0 }
+              }}
+              transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
+            >
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Possimus minima placeat accusamus illo cumque temporibus
+                voluptates culpa neque odio natus, excepturi fuga, dolores
+                dolorum esse voluptatum quia dignissimos nisi est.
+              </p>
+            </motion.section>
+          )}
+          </AnimatePresence>
+        </motion.div>
+       </article>
+     
     </div>
   );
 };
