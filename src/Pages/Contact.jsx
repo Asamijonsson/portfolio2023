f4f8637caca6b04331b1039f5fa6360e67ed942d
Asami <90178926+Asamijonsson@users.noreply.github.com>
2023-11-17 10:20:06 +0100

    contact test

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,19 +2,30 @@ import Home from "./Home"
 import colorstyle from "../Color.module.css"
 import style from "./Contact.module.css"
 const Contact =()=>{
-    return(
+    return(  
         <div role="form" id="main-content" aria-label="Contact imformation">
-            <form action="https://formsubmit.co/momoji303@gmail.com" method="POST">
+           <form method="POST" action="https://formsubmit.co/momoji303@gmail.com" enctype="multipart/form-data">
             <h3 className={colorstyle.primary}>Send message</h3>
-            {/* <label className={style.title} for="inp" aria-labelledby="inp">Title</label><br /> */}
+            <label className={style.title} for="inp" aria-labelledby="inp">Title</label><br />
             <input type="hidden" name="_next" value={<Home/>}/>
-            {/* <input className={style.input} type="text" id="inp" aria-required="true" aria-roledescription="input" required/><br /> */}
+            <input className={style.input} type="text" id="inp" aria-required="true" aria-roledescription="input" required/><br />
             <input type="email" name="email" placeholder="email" required/>
             <input type="text" name="subject" placeholder="subject" required/>
             <textarea  className={style.textarea} id="inp" cols="30" rows="10" aria-required="true" aria-roledescription="textarea" required></textarea><br />
             <button type="submit" className={style.button} >Send</button>
-            </form>
-        </div>
+            </form> 
+          </div>
+         
+    //     <div>
+    // <form method="POST" action="https://formsubmit.co/momoji303@gmail.com" enctype="multipart/form-data">
+    // <input type="email" name="email" placeholder="Your email"/>
+    // <textarea name="message" placeholder="Details of your problem"></textarea>
+    // <input type="file" name="attachment" accept="image/png, image/jpeg"/>
+    // <button type="submit">Send Test</button>
+    // </form>
+    //     </div>
+
+  
     )
 }
 
