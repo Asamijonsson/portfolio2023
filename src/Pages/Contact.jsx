8c68e95f4cc69683cb2610211c0a808400228922
Asami <90178926+Asamijonsson@users.noreply.github.com>
2023-11-17 11:50:18 +0100

    test contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -4,15 +4,13 @@ import style from "./Contact.module.css"
 const Contact =()=>{
     return(  
         <div role="form" id="main-content" aria-label="Contact imformation">
-           <form method="POST" action="https://formsubmit.co/momoji303@gmail.com">
-           <div className={style.form}>
-            <h3 className={colorstyle.primary}>Send message</h3>
+           <form method="POST" action="https://formsubmit.co/momoji303@gmail.com" className={style.form}>
+                    <h3 className={colorstyle.primary}>Send message</h3>
             <input className={style.input} type="text" id="inp" aria-required="true" aria-roledescription="input" placeholder="Subject" required/>
             <input className={style.email} type="email" name="email" placeholder="email" required/>
             <input className={style.textarea} type="text" name="message" placeholder="Write your message here" id="inp"  aria-required="true" aria-roledescription="textarea" required/>
             <button type="submit" className={style.button} >Send</button>
-            </div>
-            </form> 
+                    </form> 
             </div>
   
     )
