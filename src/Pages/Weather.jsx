8bdcdc4f6fe70bc087a3119430653e3ede4bb787
Asami <90178926+Asamijonsson@users.noreply.github.com>
2024-02-07 16:21:33 +0100

    implement weather api

--- src/Pages/Weather.jsx
+++ src/Pages/Weather.jsx
@@ -1,31 +1,53 @@
 import style from "./Weather.module.css";
 
 const weather =() =>{
-const apiKey = process.env.API_KEY
-console.log(apiKey)
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const search = async ()=> {
+const element = document.getElementById("cityInput")
+if(!element||element.value===""){
+    return 0;
+}
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${element.value}&appid=${apiKey}`;
+const response = await fetch(url)
+const data = await response.json();
+console.log(data)
+const humidity = document.getElementById("humiditiyPercent")
+const wind = document.getElementById("windRate")
+const temprature = document.getElementById("weatherTemp")
+const location = document.getElementById("weatherLocation")
+
+humidity.innerHTML = data.main.humidity+"%";
+wind.innerHTML = data.wind.speed+"km/h";
+temprature.innerHTML = data.main.temp+"K";
+location.innerHTML = data.name;
+
+}
+
 return (
 <div className={style.container}>
     <div className={style.topbar}>
-        <input type="text"/>
-        <div>
-
-        </div>
+        <input type="text"  id = "cityInput" className={style.cityInput}/>
+        <button className={style.search} onClick={()=>{search()}}>
+Search
+        </button>
     </div>
     <div className={style.weatherImg}>
 
     </div>
-    <div className={style.weatherTemp}>24°C</div>
-    <div className={style.weatherLocation}>Malmö</div>
+    <div id="weatherTemp" className={style.weatherTemp}>24°C</div>
+    <div id="weatherLocation" className={style.weatherLocation}>Malmö</div>
     <div className={style.dataContainer}>
         <div className={style.element}>
             <div className={style.data}>
-                <div className={style.humidityPercent}>64%</div>
+                <div id="humiditiyPercent" className={style.humidityPercent}>64%</div>
                 <div className={style.text}>Humidity</div>
             </div>
         </div>
         <div className={style.element}>
             <div className={style.data}>
-                <div className={style.humidityPercent}>18 km/h</div>
+                <div id="windRate" className={style.windRate}>18 km/h</div>
                 <div className={style.text}>Wind Speed</div>
             </div>
         </div>
