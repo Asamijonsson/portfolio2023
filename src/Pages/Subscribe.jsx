614073ee87974d014547e0aa1a782ce554201bd4
asamijonsson <momoji303@gmail.com>
2024-03-18 18:40:33 +0100

    fixed celcius

--- src/Pages/Subscribe.jsx
+++ src/Pages/Subscribe.jsx
@@ -7,18 +7,18 @@ const Subscribe = () => {
 
   return (
     <>
-    <p>This page is mock-up</p>
+      <p>This page is mock-up</p>
       <div className={style.subscribe}>
         <Card className={style.card}>
           <motion.p
             className={style.maintext}
             whileHover={{ scale: 1.2 }}
           >
-           <p className={style.text}> Light</p>
+            <p className={style.text}> Light</p>
           </motion.p>
           <Card.Body>
 
-            <Card.Title>¥ 50</Card.Title>
+            <Card.Title>50kr</Card.Title>
             <Card.Text>
               With supporting text below as a natural lead-in to additional content.
             </Card.Text>
@@ -26,15 +26,15 @@ const Subscribe = () => {
           </Card.Body>
         </Card>
         <Card className={style.card}>
-        <motion.p
+          <motion.p
             className={style.maintext}
             whileHover={{ scale: 1.2 }}
           >
-           <p className={style.text}>Standard</p>
+            <p className={style.text}>Standard</p>
           </motion.p>
-         
+
           <Card.Body>
-            <Card.Title>¥ 100 </Card.Title>
+            <Card.Title>100kr </Card.Title>
             <Card.Text>
               With supporting text below as a natural lead-in to additional content.
             </Card.Text>
@@ -42,15 +42,15 @@ const Subscribe = () => {
           </Card.Body>
         </Card>
         <Card className={style.card}>
-        <motion.p
+          <motion.p
             className={style.maintext}
             whileHover={{ scale: 1.2 }}
           >
-           <p className={style.text}>Premium</p>
+            <p className={style.text}>Premium</p>
           </motion.p>
-         
+
           <Card.Body>
-            <Card.Title>¥ 10,000</Card.Title>
+            <Card.Title>10,000kr</Card.Title>
             <Card.Text>
               With supporting text below as a natural lead-in to additional content.
             </Card.Text>
--- src/Pages/Weather.jsx
+++ src/Pages/Weather.jsx
@@ -1,60 +1,64 @@
 import style from "./Weather.module.css";
 
-const weather =() =>{
-const apiKey = process.env.REACT_APP_API_KEY;
+const weather = () => {
+    const apiKey = process.env.REACT_APP_API_KEY;
 
-const search = async ()=> {
-const element = document.getElementById("cityInput")
-if(!element||element.value===""){
-    return 0;
-}
+    const search = async () => {
+        const element = document.getElementById("cityInput")
+        if (!element || element.value === "") {
+            return 0;
+        }
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${element.value}&appid=${apiKey}`;
-const response = await fetch(url)
-const data = await response.json();
-console.log(data)
-const humidity = document.getElementById("humiditiyPercent")
-const wind = document.getElementById("windRate")
-const temprature = document.getElementById("weatherTemp")
-const location = document.getElementById("weatherLocation")
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${element.value}&appid=${apiKey}`;
+        const response = await fetch(url)
+        const data = await response.json();
+        console.log(data)
+        const humidity = document.getElementById("humiditiyPercent")
+        const wind = document.getElementById("windRate")
+        const temprature = document.getElementById("weatherTemp")
+        const location = document.getElementById("weatherLocation")
 
-humidity.innerHTML = data.main.humidity+"%";
-wind.innerHTML = data.wind.speed+"km/h";
-temprature.innerHTML = data.main.temp+"K";
-location.innerHTML = data.name;
+        humidity.innerHTML = data.main.humidity + "%";
+        wind.innerHTML = data.wind.speed + "km/h";
+        temprature.innerHTML = (data.main.temp - 273.15).toFixed(2) + "°C";
+        location.innerHTML = data.name;
 
-}
 
-return (
-<div className={style.container}>
-    <p className={style.text}>Check weather in your city!</p>
-    <div className={style.topbar}>
-        <input type="text"  id = "cityInput" className={style.cityInput}/>
-        <button className={style.search} onClick={()=>{search()}}>
-Search
-        </button>
-    </div>
-    <div className={style.weatherImg}>
 
-    </div>
-    <div id="weatherTemp" className={style.weatherTemp}>--K</div>
-    <div id="weatherLocation" className={style.weatherLocation}>City</div>
-    <div className={style.dataContainer}>
-        <div className={style.element}>
-            <div className={style.data}>
-                <div id="humiditiyPercent" className={style.humidityPercent}>--%</div>
-                <div className={style.text}>Humidity</div>
+    }
+
+
+
+    return (
+        <div className={style.container}>
+            <p className={style.text}>Check weather in your city!</p>
+            <div className={style.topbar}>
+                <input type="text" id="cityInput" className={style.cityInput} />
+                <button className={style.search} onClick={() => { search() }}>
+                    Search
+                </button>
+            </div>
+            <div className={style.weatherImg}>
+
+            </div>
+            <div id="weatherTemp" className={style.weatherTemp}>--°C</div>
+            <div id="weatherLocation" className={style.weatherLocation}>City</div>
+            <div className={style.dataContainer}>
+                <div className={style.element}>
+                    <div className={style.data}>
+                        <div id="humiditiyPercent" className={style.humidityPercent}>--%</div>
+                        <div className={style.text}>Humidity</div>
+                    </div>
+                </div>
+                <div className={style.element}>
+                    <div className={style.data}>
+                        <div id="windRate" className={style.windRate}>18 km/h</div>
+                        <div className={style.text}>Wind Speed</div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div className={style.element}>
-            <div className={style.data}>
-                <div id="windRate" className={style.windRate}>18 km/h</div>
-                <div className={style.text}>Wind Speed</div>
-            </div>
-        </div>
-    </div>
-</div>
-)
+    )
 }
 
 export default weather
