5836b05e13ec018db180f74902220645a7eb3289
Asami <90178926+Asamijonsson@users.noreply.github.com>
2024-06-17 19:17:14 +0200

    another minor fixed

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -4,11 +4,11 @@ import style from "./Contact.module.css"
 const Contact =()=>{
     return(  
         <div role="form" id="main-content" aria-label="Contact imformation">
-                <p>This page is mock-up</p>
+                <p className={style.bartext}>This page is mock-up</p>
            <form method="POST" action="https://formsubmit.co/momoji303@gmail.com" className={style.form}>
-                    <h3 className={colorstyle.primary}>Send message</h3>
+                    <h3 className={colorstyle.primary}>Send Message</h3>
             <input className={style.input} type="text" id="inp" aria-required="true" aria-roledescription="input" placeholder="Subject" required/>
-            <input className={style.email} type="email" name="email" placeholder="email" required/>
+            <input className={style.email} type="email" name="email" placeholder="E-mail" required/>
             <input className={style.textarea} type="text" name="message" placeholder="Write your message here" id="inp"  aria-required="true" aria-roledescription="input" required/>
             <button type="submit" className={style.button} >Send</button>
                     </form> 
