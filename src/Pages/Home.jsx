0dece951a4e57d911f44434820b5f3b1d6c31f75
Asami <90178926+Asamijonsson@users.noreply.github.com>
2024-07-04 01:20:04 +0200

    change home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,63 +1,18 @@
-import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+
 import style from "./Home.module.css";
-import { BsLinkedin } from "react-icons/bs";
-import { FiMail } from "react-icons/fi";
-import { IoIosArrowDown } from "react-icons/io";
 import Weather from "../components/Weather";
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div id="main-content">
-      <legend className={style.herobackground}>
-
-      </legend>
-      <article className={style.article}>
-        <motion.div
-          Layout
-          onClick={() => setIsOpen(!isOpen)}
-          className={style.card}
-          style={{
-            borderRadius: "1rem",
-            boxshadow: "0px 10px 30px rgba(0,0,0,0.5)",
-          }}
-        >
-          <motion.h2 Layout="position" className={style.welcomeText}>
-            Welcome to my website!{" "}<IoIosArrowDown />
-          </motion.h2>
-          <AnimatePresence>
-            {isOpen && (
-              <motion.section
-                key="content"
-                initial="collapsed"
-                animate="open"
-                exit="collapsed"
-                variants={{
-                  open: { opacity: 1, height: "auto" },
-                  collapsed: { opacity: 0, height: 0 },
-                }}
-                transition={{ duration: 0.8, ease: [0.04, 0.62, 0.23, 0.98] }}
-              >
-                <p className={style.articleText}>
-                  Hi! I'm Asami. Thank you for visiting my website. I'm a Front-end
-                  developer with a great interest in Javascript,
-                  ReactJS, HTML & CSS, UX Design, and a bit of backend NodeJS.
-                  Possesses a dedicated work ethic mind, with good communication
-                  skills. Multicultural with deep knowledge of both Swedish and
-                  Japanese cultures.
-                </p>
-                <a href="https://www.linkedin.com/in/asamijonsson/" className={style.logo} >
-                  <BsLinkedin />
-                </a>
-                <a href="mailto:momoji832@hotmail.com" className={style.logo}><FiMail /></a>
-              </motion.section>
-            )}
-          </AnimatePresence>
-        </motion.div>
-      </article>
-      <Weather />
+  return (<>
+    <div className={style.text}>
+      Hi,
+      Welcome to my website!
+      This site is for Final Fantasy XIV Crafters.
+      You can find items and recipes here.
+      I've also added a look up Pokémon, so you can find them as well.
+      Please enjoy!
     </div>
-  );
-};
-export default Home;
+    <Weather /></>)
+}
+
+export default Home
