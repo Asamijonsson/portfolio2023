ae7cce00e63751d769b986fd7e6293e56451a490
Asami <90178926+Asamijonsson@users.noreply.github.com>
2024-07-05 12:14:12 +0200

    fetch market borad from universalis

--- src/App.js
+++ src/App.js
@@ -6,67 +6,11 @@ import Footer from "./Footer";
 import Home from "./Pages/Home";
 import Aboutme from "./Pages/Aboutme";
 import ItemDetail from "./components/Itemdetail.jsx";
-import Works from "./Pages/Works";
 import Ffxiv from "./Pages/Ffxiv.jsx"
+import MarketBoard from "./Pages/MarketBoard.jsx";
 import Pokemon from "./Pages/pokemon.jsx"
 function App() {
-  const receipe = [
-    {
-      id: 0,
-      category: "Party",
-      subcategory: [
-        {
-          categoryName: "appetizers",
-          foodName: [{ name: "Kimchi" }, { name: "Harumaki" }],
-        },
-        {
-          categoryName: "main",
-          foodName: [
-            {
-              name: "Okonomiyaki",
-            },
-            { name: "Tonkatsu" },
-          ],
-        },
-        {
-          categoryName: "dessert",
-          foodName: [{ name: "Goma dango" }, { name: "Matcha ice cream" }],
-        },
-      ],
-    },
-
-    {
-      id: 1,
-      category: "Casual dinner",
-      foodName: [
-        {
-          name: "Tempura",
-        },
-        { name: "Yakitori" },
-        { name: "Gyu Tataki" },
-      ],
-    },
-    {
-      id: 3,
-      category: "Sushi",
-      foodName: [
-        {
-          name: "Maki Sushi",
-        },
-        { name: "Nigiri" },
-      ],
-    },
-    {
-      id: 4,
-      category: "Ramen",
-      foodName: [
-        {
-          name: "Tonkotsu Ramen",
-        },
-        { name: "Miso Ramen" },
-      ],
-    },
-  ];
+ 
 
   return (
     <div id="app">
@@ -79,8 +23,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="aboutme" element={<Aboutme />} />
           <Route path="item/:itemId" element={<ItemDetail />} />
-          <Route path="works" element={<Works menu={receipe} />} />
           <Route path="ffxiv" element={<Ffxiv />} />
+          <Route path="marketboard" element={<MarketBoard/>}/>
           <Route path="pokemon" element={<Pokemon/>}/>
         </Routes>
         <Footer />
--- src/NavLink.jsx
+++ src/NavLink.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { React } from "react";
+import { FaHome } from "react-icons/fa";
 import style from "./NavLink.module.css";
 import colorstyle from "./Color.module.css";
 
@@ -7,16 +8,19 @@ const NavLink = () => {
   return (
     <div className={style.navbar}>
 
-      <Link to={"/"} className={style.link} reloadDocument>
-        <h3 className={colorstyle.primary}>Home</h3>
+      <Link to="/" className={style.link} reloadDocument>
+        <h3 className={colorstyle.primary}><FaHome /></h3>
       </Link>
       <Link id="ffxiv" to="/ffxiv" className={style.link}reloadDocument>
       <h3 className={colorstyle.primary}>Look up FFXIVitems!</h3>
       </Link>   
-      <Link to={"pokemon"} className={style.link} reloadDocument>
+      <Link id="marketBoard" to="/marketboard" className={style.link}reloadDocument>
+      <h3 className={colorstyle.primary}>Look up Market Board</h3>
+      </Link>   
+      <Link to="/pokemon" className={style.link} reloadDocument>
         <h3 className={colorstyle.primary}>Look up Pokemon</h3>
       </Link>
-      <Link to={"/aboutme"} className={style.link} reloadDocument>
+      <Link to="/aboutme" className={style.link} reloadDocument>
         <h3 className={colorstyle.primary}>About</h3>
       </Link>
     </div>
