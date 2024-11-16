f93b67634966f685a5bed018a3d340f1db9f9f2f
Asami <90178926+Asamijonsson@users.noreply.github.com>
2024-11-16 11:51:05 +0100

    change the text

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,17 @@
-
 import style from "./Home.module.css";
 import Weather from "../components/Weather";
 
 const Home = () => {
-  return (<>
-    <div className={style.text}>
-      Hi,
-      Welcome to my website!
-      This site is for Final Fantasy XIV Crafters.
-      You can find items and recipes here.
-      I've also added a look up Pokémon, so you can find them as well.
-      Please enjoy!
-
-      {/* Feature update: I will implement a market board from FFXIV, where you can see item prices.  */}
+  return (
+    <>
+      <div className={style.text}>
+        Hi, Welcome to my website! This website is my hobby project. Please
+        enjoy!
+        {/* Feature update: I will implement a market board from FFXIV, where you can see item prices.  */}
       </div>
-    <Weather /></>)
-}
+      <Weather />
+    </>
+  );
+};
 
-export default Home
+export default Home;
